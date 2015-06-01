5bb0dc8b3bbc6b051b1a69da018daed1a7b72428
Robert Sullivan <r.sullivan@nature.com>
2015-06-01 17:11:41 +0100

    get zone test failing
    Moved header response test to get_zone because I want to allow
    cross origin requests using GET. It doesn't make sense to set
    that header for POST.
    
    Thinking about the behaviour of the api, if I am getting a zone
    from lat and lng lat and lng become the query parameters, they
    don't need to be posted to the server unless I am going to store them
    , which I was thinking of doing to build up the geom points of a zone.
    
    So how can I get the lat and lng saved to the db with GET?

--- tests/test_zones_app.rb
+++ tests/test_zones_app.rb
@@ -14,11 +14,17 @@ class MyAppTest < Test::Unit::TestCase
 		assert_equal 'Hello, welcome to TFL ZONESZZZZZZZZZ', last_response.body
 	end
 
+	def test_get_zone
+		get '/api/v1/zone', params={:lng => 2, :lat => 1}
+		assert last_response.ok?
+		assert_equal '*', last_response.headers['Access-Control-Allow-Origin']
+	end
+
 	
 	def test_location_post
 		post '/api/v1/location/', params={:lng => 2, :lat => 1}
 		assert last_response.ok?
-		assert_equal '*', last_response.headers['Access-Control-Allow-Origin']
+		
 		assert last_response.body.include?('Zone 1')
 	end
 end	
