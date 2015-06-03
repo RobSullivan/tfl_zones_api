94ca8eb7afa6dc81b778533a61a36164d11c72fa
Robert Sullivan <r.sullivan@nature.com>
2015-06-03 12:16:16 +0100

    Added a test for getting the zone when sending a string.
    Who knows the longitude and latitude of where they are without
    having to look it up?
    
    In addition to accepting a lat and lng this api should also
    accept a string which will be the underground station
    a user is near or knows they are near or wants to know
    what zone a station is in.

--- tests/test_zones_app.rb
+++ tests/test_zones_app.rb
@@ -14,10 +14,16 @@ class MyAppTest < Test::Unit::TestCase
 		assert_equal 'Hello, welcome to TFL ZONESZZZZZZZZZ', last_response.body
 	end
 
-	def test_get_zone
+	def test_get_zone_by_location
 		get '/api/v1/zone', params={:lng => -0.11, :lat => 51.54}
-		assert last_response.status.must_equal 200
+		assert last_response.ok?
 		assert_equal '*', last_response.headers['Access-Control-Allow-Origin']
+		assert last_response.body.include?('Zone 3')
+	end
+
+	def test_get_zone_by_station
+		get '/api/v1/zone', params={:station => 'Tulse Hill'}
+		assert last_response.ok?
 		assert last_response.body.include?('Zone 3')
 	end
 
